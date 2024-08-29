a1544ea8b2e3c6dde230073a5c51221119cf47dc
fatma ahmed <fatmaahmed222003@gmail.com>
2024-08-29 19:35:24 +0300

    add dockerCompose.yml

--- index.js
+++ index.js
@@ -2,13 +2,27 @@
 //reload environment
 require("dotenv").config();
 const express = require("express");
+const mongoose = require("mongoose");
 const app = express();
+const connection = `mongodb://${process.env.DB_USER}:${process.env.DB_PASSWORD}@mongo:${process.env.DB_PORT}`;
+mongoose.connect(connection);
+console.log(connection);
+
+mongoose.connection.on("connected", () => {
+  console.log("Connected to MongoDBb");
+});
+mongoose.connection.on("error", (err) => {
+  console.log("Error connecting to MongoDB: ");
+});
 
 const port = process.env.PORT;
 console.log("Port: " + port);
 
 app.get("/", (req, res) => {
   res.send("Hello, Docker with Express!");
+});
+app.get("/allusers", (req, res) => {
+  res.send("Hello, All users here!");
 });
 
 app.listen(port, "0.0.0.0", () => {
